f63e47a18ff2817ecb7a2a0ed86af1954286dbaa
Lambdat <oreficeantonio98@postecert.it>
2021-10-27 09:53:01 +0200

    -Aggiunta Interfaccia

---
+++ Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectWork.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectWork.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> opzioni) : base(opzioni)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+
+
+
+
+
+    }
+}
---
+++ Services/InterfaceService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectWork.Services
+{
+    //Questa interfaccia fungerà da contratto, che i vari services
+    //coinvolti implementeranno
+    public interface InterfaceService<T>
+    {
+        public List<T> GetAll();
+
+        public T Search(int id);
+        public T Search(string ssn);
+
+        public T Add(T item);
+
+        public T Delete(int id);
+
+        public T Delete(string ssn);
+
+        public T Update(int id,T item);
+        public T Update(string ssn,T item);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
