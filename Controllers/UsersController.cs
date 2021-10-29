a4a8b4874ce714b2c601972d07d94a99be535b03
DANIELE DOLCE <danieledolce00@gmail.com>
2021-10-29 16:59:43 +0200

    CRUD for users added

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectWork.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjectWork.Data;
+
+
+namespace ProjectWork.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public DataContext _ctx { get; set; }
+
+        public UsersController(DataContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        public List<User> GottaCacthEmAll()
+        {
+           return  _ctx.Users.ToList();
+        }
+    }
+}
